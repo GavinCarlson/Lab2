4e280826964e0d928f6b1f34b169ec392ee49617
GavinCarlson <122641091+GavinCarlson@users.noreply.github.com>
2023-01-23 19:37:55 -0600

    Add files via upload

---
+++ Lab2.Gavin.Carlson.py
@@ -0,0 +1,14 @@
+seconds = int(input("Enter the seconds"))
+sec = sec%86400
+hour = sec//3600
+sec% = 3600
+min = sec//60
+sec% = 60
+print("hour, min, sec")
+
+
+# Population Estimate
+
+print(334205119 + 20*24*60*60/7 - 20*24*60*60/13 + 20*24*60*60/35)
+
+pop_change = int(input("seconds since beginning of year"))
